8cbcd2978e83ca87b3d02993901e09d946ff34f5
Olivier Noumbi <olivierfaustin@yahoo.fr>
2021-05-06 14:02:28 +0100

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -19,10 +19,24 @@ class SignIn extends React.Component {
 
     //prevent the default submit function from firing
     //we want full control over what the submit will do
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        //clear the fields
-        this.setState({ email: '', password: '' });
+
+        const { email, password } = this.state;
+
+        try {
+
+            await auth.signInWithEmailAndPassword(email, password);
+            //clear the fields
+            this.setState({ email: '', password: '' });
+
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+
     }
 
     handleChange = event => {
