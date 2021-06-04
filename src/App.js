09e7d8ad4ac82a217e6396310a54b19d6741c5a3
Olivier Noumbi <olivierfaustin@yahoo.fr>
2021-06-04 10:32:48 +0100

    added cartIcon and cart-dropdown components, implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserprofileDocument } from "./firebase/firebase.utils";
-import { setCurrentUser } from '.redux/user/user.actions';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
   
